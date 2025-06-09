cd2d5e71de55937619a6112f8eccb5e102177c77
Altaf Uddin <66023452+altafuddin@users.noreply.github.com>
2025-06-09 13:47:11 +0600

    Delete backend/token_generator.py

--- backend/token_generator.py
+++
@@ -1,8 +0,0 @@
-# small_oauth_script.py (run this once if needed)
-from google_auth_oauthlib.flow import InstalledAppFlow
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-creds = flow.run_local_server(port=0)
-with open('token.json', 'w') as token_file:
-    token_file.write(creds.to_json())
-print("token.json created successfully.")
